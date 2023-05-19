7e13e6ec43b8b1a2e18caeaae02b2ba2ea59e4ab
Subhash Bose <online@subhashbose.com>
2023-05-19 11:32:54 +0000

    Add files via upload

---
+++ get_region.sh
@@ -0,0 +1,9 @@
+for file in *.udp.ovpn
+do
+	ip=`grep 'remote ' $file | head -1 | awk '{print $2}'`
+	domain=`echo $file | sed 's/.udp.ovpn//g'`
+	ipinfo=`curl -s https://ipinfo.bose.dev/$ip`
+	region=`echo "$ipinfo" |grep -E 'city|region' | sed -r 's/.*: "(.*)",/\1/g' | awk 1 ORS=', ' | rev| cut -c3- |rev`
+	
+	echo "$domain $ip    $region" |tee -a region_list.txt
+done
