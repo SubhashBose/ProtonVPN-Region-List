7cbbe0f285b43daf8317b5e798f1bdf93a988e63
Subhash Bose <online@subhashbose.com>
2023-05-28 01:33:25 -0400

    Update ping-fastest-proton.sh

--- ping-fastest-proton.sh
+++ ping-fastest-proton.sh
@@ -10,7 +10,7 @@ fi
 
 awk '{print $2}' pv.regions.tmp |  while read ip
 do
-	sh -c "echo `(echo 'round-trip min/avg/max/stddev = 0/999999/0/0 ms'; ping -c 5 -W $ptimeout $ip) | grep avg | tail -1 | cut -d '/' -f 5` $ip >> pv.ping.tmp" & 
+	sh -c "echo `(echo 'round-trip min/avg/max/stddev = 0/999999/0/0 ms'; ping -c 5 -W $ptimeout $ip) | grep avg | tail -1 | cut -d '/' -f 5`  `grep $ip pv.regions.tmp` >> pv.ping.tmp" & 
 done
 
 while [ "`wc -l pv.regions.tmp | awk '{print $1}'`" -gt "`wc -l pv.ping.tmp | awk '{print $1}'`" ]; 
@@ -18,8 +18,10 @@ do
     sleep 1
 done
 
-topip=`sort -g pv.ping.tmp | head -1 | awk '{print $2}'`
+sort -g pv.ping.tmp | head -1
 
-echo `grep $topip pv.regions.tmp` `grep $topip pv.ping.tmp | awk '{print $1}'`ms
+#topip=`sort -g pv.ping.tmp | head -1 | awk '{print $2}'`
+
+#echo `grep $topip pv.regions.tmp` `grep $topip pv.ping.tmp | awk '{print $1}'`ms
 
 rm -f pv.*.tmp 2> /dev/null
